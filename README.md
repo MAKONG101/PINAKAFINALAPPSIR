d62812ce6daf0b801f798f185f4495f7ad45ec9c
dmocsancarrera@gmail.com <dmocsancarrera@gmail.com>
2017-12-04 09:25:08 +0800

    petmalu

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# PINAKAFINALAPPSIR
+# reactMinimal
---
+++ www/js/process.js
@@ -0,0 +1,392 @@
+let process = {
+  menu:function(){
+    let content = <div className="page">
+    <div className="page-content infinite-scroll infinite-scroll-top">
+<div className="page-content contacts-content">
+  <div className="list-block contacts-block">
+    <div className="list-group">
+     <div className="navbar">
+              <div className="navbar-inner">
+                <div className="center">Chill Time</div>
+              </div>
+            </div>
+
+        <div className="list-group">
+      <ul>
+        <li className="list-group-title"></li>
+        <li><a href='#' onClick={process.LinkinPark}>
+          <div className="item-content">
+            <div className="item-inner">
+              <div className="item-title">Linkin Park</div>
+                  <div className="item-after">Details</div>
+            </div>
+          </div></a>
+        </li>
+        <li><a href='#' onClick={process.TwentyOnePilots}>
+          <div className="item-content">
+            <div className="item-inner">
+              <div className="item-title">Twenty One Pilots</div>
+                  <div className="item-after">Details</div>
+            </div>
+          </div></a>
+        </li>
+        <li><a href='#' onClick={process.ParokyaNiEdgar}>
+          <div className="item-content">
+            <div className="item-inner">
+              <div className="item-title">Parokya ni Edgar</div>
+                  <div className="item-after">Details</div>
+            </div>
+          </div></a>
+        </li>
+                <li><a href='#' onClick={process.Eraserheads}>
+          <div className="item-content">
+            <div className="item-inner">
+              <div className="item-title">Eraserheads</div>
+                  <div className="item-after">Details</div>
+            </div>
+          </div></a>
+        </li>
+        <li><a href='#' onClick={process.KhalidAmericanTeen}>
+          <div className="item-content">
+            <div className="item-inner">
+              <div className="item-title">Khalid American Teen</div>
+                  <div className="item-after">Details</div>
+            </div>`
+          </div></a>
+        </li>
+        <li><a href='#' onClick={process.Cueshe}>
+          <div className="item-content">
+            <div className="item-inner">
+              <div className="item-title">Cueshe</div>
+                  <div className="item-after">Details</div>
+            </div>
+          </div></a>
+        </li>
+        <li><a href='#' onClick={process.Kamikazee}>
+          <div className="item-content">
+            <div className="item-inner">
+              <div className="item-title">Kamikazee</div>
+                  <div className="item-after">Details</div>
+            </div>
+          </div></a>
+        </li>
+      </ul>
+    </div>
+</div>
+</div>
+</div>
+</div>
+</div>;
+    ReactDOM.render(content,document.getElementById('root'));
+  },
+  LinkinPark:function(){
+    let content = <div className="page">
+    <div className="page-content infinite-scroll infinite-scroll-top">
+<div className="page-content contacts-content">
+  <div className="list-block contacts-block">
+    <div className="list-group">
+     <div className="navbar">
+              <div className="navbar-inner">
+                <div className="center">Chill Time</div>
+              </div>
+            </div>
+            <br/><a href='#' onClick={process.menu}>back</a>
+</div>
+</div>
+</div>
+</div>
+</div>;
+      ReactDOM.render(content,document.getElementById('root'));
+  },
+  LinkinPark:function(){
+      let content = <div className="page">
+    <div className="page-content infinite-scroll infinite-scroll-top">
+<div className="page-content contacts-content">
+  <div className="list-block contacts-block">
+    <div className="list-group">
+     <div className="navbar">
+              <div className="navbar-inner">
+                <div className="center">Linkin Park</div>
+              </div>
+            </div>
+            <br /><center><img src={"img/LP.jpg"} width={"100%"}/>
+            <p> <h3>PLAYLIST</h3>
+            In the end <br />
+            Numb <br />
+            Crawling <br />
+            New Divide <br />
+            One Step Closer <br />
+            Breaking The Habit <br />
+            Castle of Glass <br />
+            Burn it Down <br />
+            Waiting for the end <br />
+            Shadow of the Day <br />
+            Bleed it out <br />
+            Lost in  the Echo <br />
+            The Catalyst <br />
+            Given Up <br />
+            From the Inside <br />
+            Lying From You <br />
+            Good Goodbye <br />
+            A Place for my head <br />
+            Faint </p></center><br />
+ <a href='#' onClick={process.menu}>back</a>
+            
+</div>
+</div>
+</div>
+</div>
+</div>;
+
+      ReactDOM.render(content,document.getElementById('root'));
+  },
+  TwentyOnePilots:function(){
+      let content = <div className="page">
+    <div className="page-content infinite-scroll infinite-scroll-top">
+<div className="page-content contacts-content">
+  <div className="list-block contacts-block">
+    <div className="list-group">
+     <div className="navbar">
+              <div className="navbar-inner">
+                <div className="center">TwentyOnePilots</div>
+              </div>
+            </div>
+            <br /><center><img src={"img/21.jpg"} width={"100%"}/>
+            <p> <h3>PLAYLIST</h3>
+            Stressed Out<br />
+            Car Radio <br />
+            Ride <br />
+            Lane Boy <br />
+            Tear in my Heart <br />
+            Fairly Local <br />
+            The Judge <br />
+            Polarize <br />
+            House of Gold <br />
+            Message Man <br />
+            Doubt <br />
+            Truce <br />
+            Kitchen Sink <br />
+            Fall Away <br />
+            Fake You Out <br />
+            Addict With a pen <br />
+            Cancer <br />
+            Trees <br />
+            Holding on to You </p></center><br />
+ <a href='#' onClick={process.menu}>back</a>
+            
+</div>
+</div>
+</div>
+</div>
+</div>;
+
+      ReactDOM.render(content,document.getElementById('root'));
+  },
+
+  ParokyaNiEdgar:function(){
+      let content = <div className="page">
+    <div className="page-content infinite-scroll infinite-scroll-top">
+<div className="page-content contacts-content">
+  <div className="list-block contacts-block">
+    <div className="list-group">
+     <div className="navbar">
+              <div className="navbar-inner">
+                <div className="center">ParokyaNiEdgar</div>
+              </div>
+            </div>
+            <br /><center><img src={"img/PNE.jpg"} width={"100%"}/>
+            <p> <h3>PLAYLIST</h3>
+            Pangarap Lang Kita<br />
+            Harana <br />
+            Halaga <br />
+            Lane Boy <br />
+            Gitara <br />
+            Lagi mong Tatandaan <br />
+            Bagsakan <br />
+            Inuman na <br />
+            Alumni Homecoming <br />
+            Huwag mo na Sana <br />
+            Buloy <br />
+            Your Song <br />
+            Silvertoes <br />
+            The Yes Yes Show <br />
+            One and Only <br />
+            Dont Touch My Birdie <br /></p></center><br />
+ <a href='#' onClick={process.menu}>back</a>
+
+</div>
+</div>
+</div>
+</div>
+</div>;
+    
+        ReactDOM.render(content,document.getElementById('root'));
+  },
+    Eraserheads:function(){
+      let content = <div className="page">
+    <div className="page-content infinite-scroll infinite-scroll-top">
+<div className="page-content contacts-content">
+  <div className="list-block contacts-block">
+    <div className="list-group">
+     <div className="navbar">
+              <div className="navbar-inner">
+                <div className="center">Eraserheads</div>
+              </div>
+            </div>
+            <br /><center><img src={"img/EE.jpg"} width={"100%"}/>
+            <p> <h3>PLAYLIST</h3>
+            Ang Huling El Bimbo<br />
+            With a Smile <br />
+            Pare Ko <br />
+            Ligaya <br />
+            Alapaap <br />
+            Magasin <br />
+            Toyang <br />
+            Inuman na <br />
+            Huwag mo ng Itanong <br />
+            Minsan <br />
+            Sembreak <br />
+            Maselang Bahaghari <br />
+            Para sa Masa <br />
+            Huwag kang Matakot <br />
+            Kaliwete <br />
+            Tindahan ni Aling Nena <br /></p></center><br />
+ <a href='#' onClick={process.menu}>back</a>
+
+</div>
+</div>
+</div>
+</div>
+</div>;
+      
+        ReactDOM.render(content,document.getElementById('root'));
+  },
+    KhalidAmericanTeen:function(){
+      let content = <div className="page">
+    <div className="page-content infinite-scroll infinite-scroll-top">
+<div className="page-content contacts-content">
+  <div className="list-block contacts-block">
+    <div className="list-group">
+     <div className="navbar">
+              <div className="navbar-inner">
+                <div className="center">Khalid American Teen</div>
+              </div>
+            </div>
+            <br /><center><img src={"img/KA.jpg"} width={"100%"}/>
+            <p> <h3>PLAYLIST</h3>
+            Young Dumb and Broke<br />
+            Location <br />
+            8TEEN <br />
+            Saved <br />
+            Coaster <br />
+            American Teen <br />
+            Lets Go<br />
+            Shot Down <br />
+            Another Sad Love Song <br />
+            Angels <br />
+            Keep Me <br />
+            Hopeless <br />
+            Therapy <br />
+            Reasons <br />
+            Winter <br /> </p></center><br />
+ <a href='#' onClick={process.menu}>back</a>
+
+</div>
+</div>
+</div>
+</div>
+</div>;
+
+
+      ReactDOM.render(content,document.getElementById('root'));
+  },
+  Cueshe:function(){
+    let content = <div className="page">
+    <div className="page-content infinite-scroll infinite-scroll-top">
+<div className="page-content contacts-content">
+  <div className="list-block contacts-block">
+    <div className="list-group">
+     <div className="navbar">
+              <div className="navbar-inner">
+                <div className="center">Eraserheads</div>
+              </div>
+            </div>
+            <br /><center><img src={"img/CU.jpg"} width={"100%"}/>
+            <p> <h3>PLAYLIST</h3>
+            Bact to Me<br />
+            Borrowed Time <br />
+            Ulan <br />
+            Cant Let You Go <br />
+            Pasensya Na <br />
+            Stay <br />
+            Pangako <br />
+            Sorry <br />
+            Bakit <br />
+            Broken Into Pieces <br />
+            Walang Yamang <br />
+            There was You <br />
+            24 hours <br />
+            Lupit <br />
+            Hump Head <br />
+            Please Naman <br /></p></center><br />
+ <a href='#' onClick={process.menu}>back</a>
+
+</div>
+</div>
+</div>
+</div>
+</div>;
+      ReactDOM.render(content,document.getElementById('root'));
+  
+  },
+  Kamikazee:function(){
+    let content = <div className="page">
+    <div className="page-content infinite-scroll infinite-scroll-top">
+<div className="page-content contacts-content">
+  <div className="list-block contacts-block">
+    <div className="list-group">
+     <div className="navbar">
+              <div className="navbar-inner">
+                <div className="center">Kamikazee</div>
+              </div>
+            </div>
+            <br /><center><img src={"img/LL.jpg"} width={"100%"}/>
+            <p> <h3>PLAYLIST</h3>
+            Halik<br />
+            Huling Sulyaw <br />
+            Martyr Nyebera <br />
+            Tagpuan <br />
+            Seksi! Swksi! <br />
+            Unang Tikim <br />
+            Pangako <br />
+            Hanggang Tingin <br />
+            Ambisosyo <br />
+            Sobrang Init <br />
+            Director's Cut <br />
+            Dobi Doo <br />
+            Unang Tagalog <br />
+            Alay <br />
+            Meron Akong Ano <br />
+            Kislap <br /></p></center><br />
+ <a href='#' onClick={process.menu}>back</a>
+
+</div>
+</div>
+</div>
+</div>
+</div>;
+      ReactDOM.render(content,document.getElementById('root'));
+  },
+
+  loading:function(){
+    let content = <div>
+          Loading...
+        </div>;
+    ReactDOM.render(content,document.getElementById('root'));
+  }
+}
+
+process.loading();
+setTimeout(function(){
+  process.menu();
+},3000);
